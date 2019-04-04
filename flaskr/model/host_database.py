57e7cc4c964bf62da471d7cbfcd51226fcf948e7
root <root@localhost.localdomain>
2019-04-03 21:08:32 -0400

    made first connection to Database

--- flaskr/model/host_database.py
+++ flaskr/model/host_database.py
@@ -1,9 +1,18 @@
 from host_model import host
+import db_config
+from psycopg2 import connect
 
 class DB_Model:
 
     def __init__(self):
         #connect to real db here
+        conn_string = "dbname='"+db_config.DB_NAME+"' user='"+db_config.DB_USER+"' password='"+db_config.DB_PASSWORD+"'"
+        conn = connect(conn_string)
+        self.cursor = conn.cursor()
+        self.cursor.execute("SELECT table_name FROM information_schema.tables")
+        print(self.cursor.fetchall())
+
+
         self.hosts = []
 
     def get_hosts(self):
