1594ff043e9ebfe30ab594a9b143cd727f797b27
Private <caryzc2@illinois.edu>
2019-04-18 03:37:17 -0500

    Revert "Revert "add view_toggle, need fix error in ribbon""
    This reverts commit 9af56cdfd327cde978d5ba00b81573bab8fca588.

--- flaskr/model/host_database.py
+++ flaskr/model/host_database.py
@@ -5,6 +5,7 @@ from psycopg2 import connect
 
 class DB_Model:
 
+
     def __init__(self):
         #connect to real db here
         try:
@@ -13,14 +14,22 @@ class DB_Model:
             self.cursor = conn.cursor()
             self.cursor.execute("SELECT table_name FROM information_schema.tables")
         except :
-            print("DB connect failed") 
+            print("DB connect failed")
             self.connected = False
 
 
         self.hosts = []
+        self.view_toggle = 'ports'
 
     def get_hosts(self):
+        if self.view_toggle == "ports":
+            self.hosts.sort(key=lambda x: x.num_ports, reverse=True)
+        else:
+            self.hosts.sort(key=lambda x: x.num_ports, reverse=True)
         return self.hosts
+
+    def get_view_toggle(self):
+        return self.view_toggle
 
     #update from the DB
     def update_hosts(self):
@@ -32,7 +41,8 @@ class DB_Model:
         rows = self.cursor.fetchall()
         for row in rows:
             host_ports = self.get_host_port_info(row[0]) #id
-            self.hosts.append(host(ident=row[0],ip=row[1], OS=row[2], ports=host_ports, purpose=row[4], comments=row[5])) 
+            self.hosts.append(host(ident=row[0],ip=row[1], OS=row[2], ports=host_ports, purpose=row[4], comments=row[5]))
+        self.hosts.sort(key=lambda x: x.num_ports, reverse=True)
         return True
 
     def test_update_hosts(self, num=1):
@@ -41,7 +51,7 @@ class DB_Model:
 
     def get_host_port_info(self, host_id):
         q_string = "SELECT port, name, info, state FROM public.services WHERE services.host_id = " + str(host_id)
-        self.cursor.execute(q_string) 
+        self.cursor.execute(q_string)
         port_list = self.cursor.fetchall()
         if port_list is None:
             port_list = []
